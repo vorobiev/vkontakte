1cdd46001ca55f309bc67b852c073c66697ded44
Antony_Vorobiev <me@antonvorobiev.ru>
2013-02-18 13:30:55 +0400

    Clean install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vkontakte::Application.config.secret_token = '4237e7c529fec027ddd087a25c8ca4de40150587f76cb298571b2d9c9aeea5db1fe1a81c27732831bfb64028101cac0a38571c7b6e0b2c82f28591df5a0581f2'
